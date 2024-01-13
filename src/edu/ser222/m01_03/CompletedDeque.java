9207f7748c284307d9e56825de17f4bf8bc645ee
johnst101 <tylercjohnson16@gmail.com>
2024-01-12 20:57:48 -0600

    starting module 2 project with new branch

---
+++ src/edu/ser222/m01_03/CompletedDeque.java
@@ -0,0 +1,49 @@
+package edu.ser222.m01_03;
+
+/**
+ * This program provides an implementation of the Deque interface. Also provides a main that
+ * demonstrates it.
+ * 
+ * @author (your name), Acuna
+ * @version (version)
+ */
+
+
+public class CompletedDeque<Item> implements Deque<Item> {
+
+    //TODO: implement all the methods
+
+    /**
+     * Program entry point for deque. 
+     * @param args command line arguments
+     */    
+    public static void main(String[] args) {
+        CompletedDeque<Integer> deque = new CompletedDeque<>();
+
+        //standard queue behavior
+        deque.enqueueBack(3);
+        deque.enqueueBack(7);
+        deque.enqueueBack(4);
+        deque.dequeueFront();        
+        deque.enqueueBack(9);
+        deque.enqueueBack(8);
+        deque.dequeueFront();
+        System.out.println("size: " + deque.size());
+        System.out.println("contents:\n" + deque.toString());   
+
+        //deque features
+        System.out.println(deque.dequeueFront());        
+        deque.enqueueFront(1);
+        deque.enqueueFront(11);                         
+        deque.enqueueFront(3);                 
+        deque.enqueueFront(5);         
+        System.out.println(deque.dequeueBack());
+        System.out.println(deque.dequeueBack());        
+        System.out.println(deque.last());                
+        deque.dequeueFront();
+        deque.dequeueFront();        
+        System.out.println(deque.first());        
+        System.out.println("size: " + deque.size());
+        System.out.println("contents:\n" + deque.toString());            
+    }
+} 
