c1abb03d6db221a71a0a540d29c9087de499e791
johnst101 <tylercjohnson16@gmail.com>
2024-01-12 21:55:04 -0600

    added method skeletons and completed node class first draft

--- src/edu/ser222/m01_03/CompletedDeque.java
+++ src/edu/ser222/m01_03/CompletedDeque.java
@@ -1,17 +1,100 @@
 package edu.ser222.m01_03;
 
+import java.util.NoSuchElementException;
+
 /**
  * This program provides an implementation of the Deque interface. Also provides a main that
  * demonstrates it.
  * 
- * @author (your name), Acuna
- * @version (version)
+ * @author Tyler Johnson, Acuna
+ * @version 1.0
  */
 
 
 public class CompletedDeque<Item> implements Deque<Item> {
+    private CompletedNode<Item> first;
+    private CompletedNode<Item> last;
+    private int size;
+
 
     //TODO: implement all the methods
+    public CompletedDeque () {
+
+    }
+
+    public void enqueueFront (Item element) {
+
+    }
+
+    public void enqueueBack (Item element) {
+
+    }
+
+    public Item dequeueFront() throws NoSuchElementException {
+        // throws NoSuchElementException if deque is empty
+    }
+
+    public Item dequeueBack() throws NoSuchElementException {
+        // throws NoSuchElementException if deque is empty
+    }
+
+    public Item first () throws NoSuchElementException {
+        return first;
+    }
+
+    public Item last () throws NoSuchElementException {
+        return last;
+    }
+
+    public boolean isEmpty () {
+        return size == 0;
+    }
+
+    public int size () {
+        return size;
+    }
+
+    public class CompletedNode<Item> {
+        private CompletedNode<Item> next;
+        private CompletedNode<Item> previous;
+        private Item element;
+
+        public CompletedNode () {
+            this.next = null;
+            this.previous = null;
+            this.element = null;
+        }
+
+        public CompletedNode (Item elem) {
+            this.next = null;
+            this.previous = null;
+            this.element = elem;
+        }
+
+        public CompletedNode<Item> getNext () {
+            return next;
+        }
+
+        public CompletedNode<Item> getprevious () {
+            return previous;
+        }
+
+        public void setNext (CompletedNode<Item> nextNode) {
+            next = nextNode;
+        }
+
+        public void setPrevious (CompletedNode<Item> previousNode) {
+            previous = previousNode;
+        }
+
+        public Item getElement () {
+            return element;
+        }
+
+        public void setElement (Item elem) {
+            element = elem;
+        }
+    }
 
     /**
      * Program entry point for deque. 
