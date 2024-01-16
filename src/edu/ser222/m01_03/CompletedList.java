3fc9631faf14df7156b8b22123c3732b7335491b
Tyler Johnson <tylerjohnson@Emilys-MacBook-Air.local>
2024-01-15 22:30:45 -0600

    added files to setup module 3 homework

---
+++ src/edu/ser222/m01_03/CompletedList.java
@@ -0,0 +1,19 @@
+package edu.ser222.m01_03;
+
+import java.util.Iterator;
+
+/**
+ * CompletedList represents an implementation of a list.
+ *
+ * @author (your name), Acuna
+ * @version (version)
+ */
+public class CompletedList<T> implements ListADT<T>, Iterable<T> {
+
+    //The following three variables are a suggested start if you are using a list implementation.
+    //protected int count;
+    //protected int modChange;
+    //protected DoubleLinearNode<T> head, tail;
+
+    //TODO: implement this!
+}
---
+++ src/edu/ser222/m01_03/CompletedOrderedList.java
@@ -0,0 +1,15 @@
+package edu.ser222.m01_03;
+
+/**
+ * CompletedOrderedList represents an implementation of an ordered list that builds on
+ * CompletedList.
+ *
+ * @author (your name), Acuna
+ * @version (version)
+ */
+public class CompletedOrderedList<T extends Comparable<T>> extends CompletedList<T>
+         implements OrderedListADT<T> {
+
+    //TODO: implement this!
+
+}
