50bce30b1508941993d9912b605ab9fdc0374cd4
Tyler Johnson <tylerjohnson@Emilys-MacBook-Air.local>
2024-01-16 18:19:12 -0600

    added method stubs for implementing ListADT and OrderedListADT interfaces

--- src/edu/ser222/m01_03/CompletedList.java
+++ src/edu/ser222/m01_03/CompletedList.java
@@ -1,19 +1,71 @@
 package edu.ser222.m01_03;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
  * CompletedList represents an implementation of a list.
  *
- * @author (your name), Acuna
- * @version (version)
+ * @author Tyler Johnson, Acuna
+ * @version 1.0
  */
 public class CompletedList<T> implements ListADT<T>, Iterable<T> {
-
     //The following three variables are a suggested start if you are using a list implementation.
+    //TODO: add member variables
     //protected int count;
     //protected int modChange;
     //protected DoubleLinearNode<T> head, tail;
 
-    //TODO: implement this!
+    public CompletedList<T>() { //TODO: complete default constructor
+
+    }
+
+    public CompletedList<T>() { //TODO: complete input fed constructor
+
+    }
+
+    @Override
+    public T removeFirst() throws NoSuchElementException { //TODO: complete removeFirst
+        return null;
+    }
+
+    @Override
+    public T removeLast() throws NoSuchElementException { //TODO: complete removeLast
+        return null;
+    }
+
+    @Override
+    public T remove(T element) { //TODO: complete remove
+        return null;
+    }
+
+    @Override
+    public T first() { //TODO: complete first
+        return null;
+    }
+
+    @Override
+    public T last() { //TODO: complete last
+        return null;
+    }
+
+    @Override
+    public boolean contains(T target) { //TODO: complete contains
+        return false;
+    }
+
+    @Override
+    public boolean isEmpty() { //TODO: complete isEmpty
+        return false;
+    }
+
+    @Override
+    public int size() { //TODO: complete size;
+        return 0;
+    }
+
+    @Override
+    public Iterator<T> iterator() { //TODO: complete iterator
+        return null;
+    }
 }
--- src/edu/ser222/m01_03/CompletedOrderedList.java
+++ src/edu/ser222/m01_03/CompletedOrderedList.java
@@ -4,12 +4,19 @@ package edu.ser222.m01_03;
  * CompletedOrderedList represents an implementation of an ordered list that builds on
  * CompletedList.
  *
- * @author (your name), Acuna
- * @version (version)
+ * @author Tyler Johnson, Acuna
+ * @version 1.0
  */
 public class CompletedOrderedList<T extends Comparable<T>> extends CompletedList<T>
          implements OrderedListADT<T> {
+    //TODO: add member variables
 
-    //TODO: implement this!
+    //TODO: complete default constructor
 
+    //TODO: complete inputted constructor
+
+    @Override
+    public void add(T element) throws NullPointerException { //TODO: complete add
+
+    }
 }
