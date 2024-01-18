270ad4291e884f17f62cfd4b6452d2c58537efe5
johnst101 <tylercjohnson16@gmail.com>
2024-01-17 20:34:35 -0600

    fixed constructor issue. Was creating a duplicate list

--- src/edu/ser222/m01_03/CompletedOrderedList.java
+++ src/edu/ser222/m01_03/CompletedOrderedList.java
@@ -1,58 +1,55 @@
 package edu.ser222.m01_03;
 
 /**
- * CompletedOrderedList represents an implementation of an ordered list that builds on
- * CompletedList.
+ * CompletedOrderedlist represents an implementation of an ordered list that builds on
+ * Completedlist.
  *
  * @author Tyler Johnson, Acuna
  * @version 1.0
  */
 public class CompletedOrderedList<T extends Comparable<T>> extends CompletedList<T>
          implements OrderedListADT<T> {
-    //TODO: add MORE???? member variables
-    private CompletedList<T> list;
-
-    public CompletedOrderedList() {
-        list = new CompletedList<>();
-    }
 
     @Override
     public void add(T element) throws NullPointerException {
+        if (element == null) {
+            throw new NullPointerException();
+        }
         DoubleLinearNode<T> newNode = new DoubleLinearNode<>(element, null, null);
-        if (list.isEmpty()) {
-            list.head = newNode;
-            list.tail = newNode;
-            list.count++;
-            list.modChange++;
+        if (isEmpty()) {
+            head = newNode;
+            tail = newNode;
+            count++;
+            modChange++;
         } else {
-            DoubleLinearNode<T> curNode = new DoubleLinearNode<>(list.head.getElement(), list.head.getNext(), list.head.getPrevious());
+            DoubleLinearNode<T> curNode = new DoubleLinearNode<>(head.getElement(), head.getNext(), head.getPrevious());
 
-            while (list.iterator().hasNext()) {
+            while (iterator().hasNext()) {
                 if (element.compareTo(curNode.getElement()) <= 0) {
-                    if (curNode.equals(list.head)) {
-                        newNode.setNext(list.head);
-                        list.head.setPrevious(newNode);
-                        list.head = newNode;
-                        list.count++;
-                        list.modChange++;
-                    } else if (curNode.equals(list.tail)) {
-                        newNode.setPrevious(list.tail);
-                        list.tail.setNext(newNode);
-                        list.tail = newNode;
-                        list.count++;
-                        list.modChange++;
+                    if (curNode.equals(head)) {
+                        newNode.setNext(head);
+                        head.setPrevious(newNode);
+                        head = newNode;
+                        count++;
+                        modChange++;
+                    } else if (curNode.equals(tail)) {
+                        newNode.setPrevious(tail);
+                        tail.setNext(newNode);
+                        tail = newNode;
+                        count++;
+                        modChange++;
                     } else {
                         newNode.setNext(curNode);
                         newNode.setPrevious(curNode.getPrevious());
                         curNode.getPrevious().setNext(newNode);
                         curNode.setPrevious(newNode);
-                        list.count++;
-                        list.modChange++;
+                        count++;
+                        modChange++;
                     }
                 }
 
                 curNode = curNode.getNext();
-                list.iterator().next();
+                iterator().next();
             }
         }
     }
