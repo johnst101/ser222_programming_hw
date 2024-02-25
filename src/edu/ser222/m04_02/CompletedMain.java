d553680733196fbd726e2626a12aec2b2385502f
johnst101 <tylercjohnson16@gmail.com>
2024-02-24 18:32:53 -0600

    Completed Main method besides buildOrderString.

--- src/edu/ser222/m04_02/CompletedMain.java
+++ src/edu/ser222/m04_02/CompletedMain.java
@@ -16,6 +16,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
@@ -28,15 +29,65 @@ public class CompletedMain implements KanjiMain {
     //TODO: implement interface methods.
 
     public HashMap<Integer, String> loadKanji(String filename, EditableDiGraph graph) {
-        throw new java.lang.UnsupportedOperationException("TODO!");
+        HashMap<Integer, String> kanjiMap = new HashMap<>();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("resources/" + filename));
+            String line = reader.readLine();
+            char comCheck;
+            while (line != null) {
+                reader.mark(1);
+                comCheck = (char) reader.read();
+                if (comCheck == '#') {
+                    reader.reset();
+                    reader.readLine();
+                    continue;
+                }
+                reader.reset();
+                line = reader.readLine();
+                String[] lineArr = line.split("\t");
+                int id = Integer.parseInt(lineArr[0]);
+                String kanjiChar = lineArr[1];
+                kanjiMap.put(id, kanjiChar);
+                graph.addVertex(id);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return kanjiMap;
     }
 
     public void loadDataComponents(String filename, EditableDiGraph graph) {
-        throw new java.lang.UnsupportedOperationException("TODO!");
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("resources/" + filename));
+            String line = reader.readLine();
+            char comCheck;
+            while (line != null) {
+                reader.mark(1);
+                comCheck = (char) reader.read();
+                if (comCheck == '#') {
+                    reader.reset();
+                    reader.readLine();
+                    continue;
+                }
+                reader.reset();
+                line = reader.readLine();
+                String[] lineArr = line.split("\t");
+                int char1 = Integer.parseInt(lineArr[0]);
+                int char2 = Integer.parseInt(lineArr[1]);
+                graph.addEdge(char1, char2);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public String buildOrderString(EditableDiGraph graph, TopologicalSort topSort, HashMap<Integer, String> kanjiMap) {
-         throw new java.lang.UnsupportedOperationException("TODO!");
+        // TODO: implement
+        throw new java.lang.UnsupportedOperationException("TODO!");
     }
 
     public static void main(String[] args) {
@@ -52,9 +103,9 @@ public class CompletedMain implements KanjiMain {
         HashMap<Integer, String> kanjiMap = driver.loadKanji(FILENAME_KANJI, graph);
         driver.loadDataComponents(FILENAME_COMPONENTS, graph);
 
-        TopologicalSort intuitive = new IntuitiveTopological(graph);
+        //TopologicalSort intuitive = new IntuitiveTopological(graph);
 
-        System.out.println(driver.buildOrderString(graph, intuitive, kanjiMap));
+        //System.out.println(driver.buildOrderString(graph, intuitive, kanjiMap));
 
         /***************************************************************************
          * END - CORE DRIVER LOGIC, DO NOT MODIFY                                  *
